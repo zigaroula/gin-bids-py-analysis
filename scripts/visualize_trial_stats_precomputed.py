5a8a16033b325bd498e61e107681a450b6be0354
Benjamin BONTEMPS <benjaminbontemps@gmail.com>
2026-04-02 11:14:58 +0200

    Changed wrong channel names

--- scripts/visualize_trial_stats_precomputed.py
+++ scripts/visualize_trial_stats_precomputed.py
@@ -43,22 +43,22 @@ GROUP_PARAMS = TrialStatsGroupParams(
             "epi11": ["X05", "X07", "X06"],
             "epi12": ["Ap02"],
             "epi14": ["Xp04"],
-            "epi17": ["II08"],
+            "epi17": ["II8"],
             "epi18": ["X06"],
             "epi19": ["IMD2"],
-            "epi21": ["XS07", "XD07", "XD04", "XD08"],
-            "epi22": ["XS08"],
-            "epi23": ["EL02"]
+            "epi21": ["XS7", "XD7", "XD4", "XD8"],
+            "epi22": ["XS8"],
+            "epi23": ["EL2"]
         },
         "vaINS": {
-            "epi04": ["Bp2"],
+            "epi04": ["Bp02"],
             "epi08": ["IA2", "IA4"],
             "epi11": ["X04"],
             "epi14": ["Xp02"],
-            "epi17": ["II02"],
+            "epi17": ["II2"],
             "epi18": ["Y02", "X02"],
-            "epi21": ["XS04"],
-            "epi22": ["XS02", "YS02"]
+            "epi21": ["XS4"],
+            "epi22": ["XS2", "YS2"]
         },
     }
 )
